4abd589c9765f4a93d9e76c87a92b28745796713
Isaac Costa <isaac.systemilc@gmail.com>
2022-10-30 14:24:57 -0300

    criado condição de comparação entre chute e número secreto

--- adicinhacao.c
+++ adicinhacao.c
@@ -11,10 +11,19 @@ int main(int argc, char** argv) {
 	printf("\n \n** Ola, seja bem vindo ao jogo de adivinha **");
 	printf("\n \n*********************************************");
 	
-	printf("\n Qual é o seu chute? ");
+	printf("\n Qual e  o seu chute? ");
 	scanf("%d", &chute);
 	
 	printf("\n Seu chute foi %d.", chute);
+	
+	if(chute == numerosecreto){
+			printf("\n Parabens, voce ganhou");
+	}else{
+		printf("\n Ops!!, voce errou!!");
+	}
+	
+	
+
 	
 	
 	return 0;
